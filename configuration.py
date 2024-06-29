bb409cebd61941b0c9ec6bcf8f6d8accec84bae8
Ashraf Hashash <ashrafhashashx@gmail.com>
2024-06-29 02:57:43 +0200

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+MIN_ROWS: int = 3
+INITIAL_ROWS: int = 5
+MAX_ROWS: int = 30
+
+MIN_COLS: int = 4
+INITIAL_COLS: int = 7
+MAX_COLS: int = 40
+
+MAX_MINES_LIMITER_FACTOR: float = 1.5
+
+
+def meaningful_number_of_max_mines(num_rows: int, num_cols: int):
+    return min(round(MAX_MINES_LIMITER_FACTOR * (num_rows + num_cols - 1)), num_rows * num_cols - 1)
+
+
+MIN_MINES: int = 0
+INITIAL_MINES: int = 9
+MAX_MINES: int = meaningful_number_of_max_mines(MAX_ROWS, MAX_COLS)
+
+
+BUTTON_WIDTH_FACTOR = 0.90
+BUTTON_HEIGHT_FACTOR = 0.96
+
+MINE_SYMBOL = 'X'
+FLAG_SYMBOL = '!'
+
+
+# noinspection SpellCheckingInspection
+class Coloring(Enum):
+    NORMALIZE = ('lightblue', 'black')
+    FLAG = ('red', 'black')
+    EMPHASIZE = ('white', 'black')
+    WIN = ('lightgreen', 'black')
+    LOSS = ('orangered', 'black')
